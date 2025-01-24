0bfd020b236a66369fae0009991769ada085df77
Vinicius Lacorte Galina <vlacortegalina@gmail.com>
2025-01-24 14:58:51 +0100

    cleaned the page!

--- app.py
+++ app.py
@@ -5,23 +5,6 @@ import pandas as pd
 # TaxiFareModel front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
 datentime = st.text_input('Date and time', '2014-07-06 19:18:00')
 pickup_long = st.text_input('Pickup longitude', -73.950655)
 pickup_lat = st.text_input('Pickup latitude', 40.783282)
@@ -30,31 +13,12 @@ dropoff_lat = st.text_input('Dropoff latitude', 40.769802)
 n_passengers = st.text_input('Number of passengers', 2)
 
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
 url = 'https://taxifare-1019856209529.europe-west1.run.app/predict'
 
 if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
 params = {
     'pickup_datetime': datentime,
     'pickup_longitude': pickup_long,
@@ -65,9 +29,9 @@ params = {
 }
 
 response = requests.get(url, params)
-data = response.json()
+data = response.json()['fare']
 
-st.write(data)
+st.write(f'### The price of your ride is estimated in: {round(data,2)}U$')
 
 def get_map_data():
 
